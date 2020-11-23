aee7095443948bc707cba4cd41fd9bd5b6981c91
김소정 <sojeong7524@gmail.com>
2020-11-23 18:35:55 +0900

    노드, 간선 그리기 구현 진짜 완료

--- app/src/main/java/com/example/kruscalprim/MainActivity.java
+++ app/src/main/java/com/example/kruscalprim/MainActivity.java
@@ -22,14 +22,12 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
     private ImageView circleView[];
     private ImageView lineView[];
-    private float x, y; // 마우스 커서 x, y 좌표
+    private float x[], y[]; // 마우스 커서 x, y 좌표
     private View myView;
     private int i = 1; // 노드 숫자
     private int j = 1; // 간선 숫자
-    private EditText start;
-    private EditText stop;
-    private EditText weight;
-    private int nodePoint[];
+    private int start, stop, weight;
+    private EditText editText, editText2, editText3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,11 +59,13 @@ public class MainActivity extends AppCompatActivity {
         }
 
         // 나머지 id 정리
-        start = findViewById(R.id.editTextNumber);     // 출발점
-        stop = findViewById(R.id.editTextNumber2);     // 도착점
-        weight = findViewById(R.id.editTextNumber3);   // 가중치
+        editText = findViewById(R.id.editTextNumber);     // 출발점
+        editText2 = findViewById(R.id.editTextNumber2);     // 도착점
+        editText3 = findViewById(R.id.editTextNumber3);   // 가중치
+
         Button button = findViewById(R.id.button);     // 입력
-        nodePoint = new int[2];
+        x = new float[14];
+        y = new float[14];
 
         // 화면 터치 시
         myView = findViewById(R.id.relativeLayout);
@@ -76,7 +76,6 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         });
-
         // 입력 버튼 클릭 시
         button.setOnClickListener(onClickListener);
     }
@@ -87,6 +86,10 @@ public class MainActivity extends AppCompatActivity {
         public void onClick(View v) {
             switch(v.getId()){
                 case(R.id.button):
+                    start = Integer.parseInt(editText.getText().toString());
+                    stop = Integer.parseInt(editText2.getText().toString()) * 2;
+                    weight = Integer.parseInt(editText3.getText().toString());
+
                     drawLine();
                     break;
             }
@@ -96,8 +99,8 @@ public class MainActivity extends AppCompatActivity {
     // 원 그리는 함수
     public void drawCircle(MotionEvent event) {
         if(i < 14) {
-            x = event.getX();
-            y = event.getY();
+            x[i] = event.getX();
+            y[i] = event.getY();
 
             Paint circlePaint = new Paint();
             circlePaint.setStyle(Paint.Style.FILL);
@@ -109,11 +112,11 @@ public class MainActivity extends AppCompatActivity {
             Bitmap bitmap = Bitmap.createBitmap(myView.getWidth(), myView.getHeight(), Bitmap.Config.ARGB_8888);
 
             Canvas canvas = new Canvas(bitmap);
-            canvas.drawCircle(x, y, 100, circlePaint);
+            canvas.drawCircle(x[i], y[i], 100, circlePaint);
             if(i < 13) {
-                canvas.drawText(String.valueOf(i / 2), x - 28, y + 40, textPaint);
+                canvas.drawText(String.valueOf(i / 2), x[i] - 28, y[i] + 40, textPaint);
             } else {
-                canvas.drawText(String.valueOf(7), x - 28, y + 40, textPaint);
+                canvas.drawText(String.valueOf(7), x[i] - 28, y[i] + 40, textPaint);
             }
             circleView[i].setImageBitmap(bitmap);
             i++;
@@ -125,16 +128,18 @@ public class MainActivity extends AppCompatActivity {
         Paint linePaint = new Paint();
         linePaint.getStrokeWidth();
         linePaint.setColor(Color.BLACK);
+        linePaint.setStrokeWidth(5f);
 
         Paint textPaint = new Paint();
-        textPaint.setColor(Color.WHITE);
+        textPaint.setColor(Color.BLACK);
         textPaint.setTextSize(50);
 
         Bitmap bitmap = Bitmap.createBitmap(myView.getWidth(), myView.getHeight(), Bitmap.Config.ARGB_8888);
 
         Canvas canvas = new Canvas(bitmap);
-        canvas.drawLine(20, 20, 1000, 1000, linePaint);
-        canvas.drawText(String.valueOf(weight), 80, 80, textPaint);
+        Log.e("log", "x: " + x[start]);
+        canvas.drawLine(x[start], y[start], x[stop], y[stop], linePaint);
+        canvas.drawText(String.valueOf(weight), (x[start] + x[stop]) / 2, (y[start] + y[stop]) / 2, textPaint);
         lineView[j].setImageBitmap(bitmap);
         j++;
     }
